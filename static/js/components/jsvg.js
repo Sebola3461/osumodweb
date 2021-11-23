fa68212084c770f8766fe98f87933890080512c3
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2021-11-23 18:00:21 -0300

    Add new custom element to display svg icons

---
+++ static/js/components/jsvg.js
@@ -0,0 +1,20 @@
+class JSVG extends HTMLElement {
+    constructor() {
+        super()
+        this.shadow = this.attachShadow({ mode: "open" })
+        this.icon = this.getAttribute("icon");
+        this.color = this.getAttribute("color");
+        this.w = this.getAttribute("w");
+        this.h = this.getAttribute("h");
+        this.style.color = this.color;
+        this.style.width = `${this.w}px`;
+        this.style.height = `${this.h}px`;
+        fetch(`/static/assets/svg/${this.icon}.svg`).then(r => {
+            return r.text()
+        }).then((svg) => {
+            this.shadow.innerHTML = svg;
+        })
+    }
+}
+
+customElements.define("jsvg-icon", JSVG)
